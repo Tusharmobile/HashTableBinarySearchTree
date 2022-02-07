58e8b9cf1c9914b12bd7357f5a1b94a79eac91c3
tushar <tusharshitre123@gmail.com>
2022-02-07 18:35:06 +0530

    adding program for add

---
+++ HashTableBinarySearchTree/HashTableBinarySearchTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table And Binary Search Tree");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+
+            Console.WriteLine(hash.GetV("5"));
+            Console.WriteLine(hash.GetV("0"));
+            Console.ReadLine();
+        }
+    }
+}
